bb11224303da7a7aa3eb7dd853d94cdb706b0fa3
roshan-bajgain <roshan.bazgain@gmail.com>
2024-02-14 16:49:52 +0545

    Create api\v1 for external api and also set routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  resources :menu_items
-  resources :menu_items, only: [:index, :show, :create, :update, :destroy]
+  namespace :api do
+    namespace :v1 do
+      resources :menu_items, only: [:index, :show, :create, :update, :destroy]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
