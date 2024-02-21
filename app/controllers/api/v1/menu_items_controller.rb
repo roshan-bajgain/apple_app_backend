59aafba0a15050133bccd2dbb1882dfb5e9775c3
roshan-bajgain <roshan.bazgain@gmail.com>
2024-02-21 15:35:00 +0545

    Add relation between user and menu items

--- app/controllers/api/v1/menu_items_controller.rb
+++ app/controllers/api/v1/menu_items_controller.rb
@@ -1,4 +1,4 @@
-class MenuItemsController < ApplicationController
+class Api::V1::MenuItemsController < ApplicationController
   before_action :set_menu_item, only: %i[ show edit update destroy ]
 
   # GET /menu_items or /menu_items.json
--- app/models/menu_item.rb
+++ app/models/menu_item.rb
@@ -1,2 +1,3 @@
 class MenuItem < ApplicationRecord
+    belongs_to :users
 end
